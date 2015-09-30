90e15d1455b1349c3cdc751a75191f96540a3bb9
rmrhz <rupaheizu@hakz.co>
2015-09-30 17:20:09 +0800

    Added documentation and needed implementational functions

--- src/Pugs/Application/Http/Kernel.php
+++ src/Pugs/Application/Http/Kernel.php
@@ -4,14 +4,31 @@ namespace Pugs\Application\Http;
 
 use Pugs\Application\Core;
 use League\Route\RouteCollection;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class Kernel
 {
 
+	/**
+	 * Core container
+	 *
+	 * @var Pugs\Application\Core
+	 */
 	protected $core;
 
+	/**
+	 * League routing container
+	 *
+	 * @var League\Route\RouteCollection
+	 */
 	protected $route;
 
+	/**
+	 * Route middleware
+	 *
+	 * @var array
+	 */
 	protected $middleware;
 
 	public function __construct(Core $core, RouteCollection $route)
@@ -23,4 +40,27 @@ class Kernel
 	}
 
 	// function for gathering middleware for a specific route/request
+
+	/**
+	 * Handles incoming HTTP requuest.
+	 *
+	 * @param \Symfony\Component\HttpFoundation\Request $request
+	 * @return \Symfony\Component\HttpFoundation\Response $response
+	 */
+	public function handle(Request $request)
+	{
+		// round robin through the middleware
+	}
+
+	/**
+     * Call the terminate method on any terminable middleware.
+     *
+     * @param  \Symfony\Component\HttpFoundation\Request  $request
+     * @param  \Symfony\Component\HttpFoundation\Response  $response
+     * @return void
+     */
+	public function terminate(Request $request, Response $response)
+	{
+
+	}
 }
