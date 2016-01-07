b02233a5a3686d03222c8fa76bcac07a30032d2b
rmrhz <rmrhz@tfwno.gf>
2016-01-07 23:29:04 +0800

    Fixed type for env configured database password

--- config/production/database.php
+++ config/production/database.php
@@ -11,7 +11,7 @@ return [
 			'host' => getenv('DB_HOSTNAME'),
 			'database' => getenv('DB_DATABASE'),
 			'username' => getenv('DB_USERNAME'),
-			'password' => getenv('DB_PASWORD'),
+			'password' => getenv('DB_PASSWORD'),
 			'charset' => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix' => ''
