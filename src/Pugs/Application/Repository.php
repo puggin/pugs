712a23f63010f961d78818e790fecfb5ca0c5f18
rmrhz <rmrhz@tfwno.gf>
2016-01-09 21:01:13 +0800

    Added entity property in Repository for easier Entity manipulation

--- src/Pugs/Application/Repository.php
+++ src/Pugs/Application/Repository.php
@@ -6,6 +6,34 @@ abstract class Repository
 {
 
 	/**
+	 * Entity container
+	 *
+	 * @var Object
+	 */
+	protected $entity;
+
+	/**
+	 * Sets the current entity
+	 *
+	 * @param Object $entity
+	 * @return void
+	 */
+	public function setEntity($entity)
+	{
+		$this->entity = $entity;
+	}
+
+	/**
+	 * Returns the current configured entity
+	 *
+	 * @return $entity
+	 */
+	public function getEntity()
+	{
+		return $this->entity;
+	}
+
+	/**
 	 * Mass assignment based on fillable, hidden, and guarded attributes
 	 *
 	 * @param object $model
