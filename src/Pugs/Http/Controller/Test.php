95f7ea4dde4fba37e1dcc0e157a2e2fe276612fc
rmrhz <rmrhz@tfwno.gf>
2016-01-10 23:57:11 +0800

    Removed auth middleware for further testing

--- src/Pugs/Http/Controller/Test.php
+++ src/Pugs/Http/Controller/Test.php
@@ -7,12 +7,10 @@ use League\Route\Http\JsonResponse\Ok;
 
 class Test extends Controller
 {
-	protected $middleware = [
-		'Pugs\Http\Middleware\AuthBearer',
-	];
 
 	public function getIndex()
 	{
 		return new Ok($this->getRequest()->headers->all());
 	}
+
 }
