574389b1be533c476186bc41f5e6e050e2e00abc
rmrhz <rmrhz@tfwno.gf>
2016-01-22 12:06:00 +0900

    Altered namespace

--- src/Pugs/Adapter/LeagueAdapter/Adapter.php
+++ src/Pugs/Adapter/LeagueAdapter/Adapter.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Pugs\LeagueAdapter;
+namespace Pugs\Adapter\LeagueAdapter;
 
 use League\Route\RouteCollection;
 
--- src/Pugs/Adapter/LeagueAdapter/Provider.php
+++ src/Pugs/Adapter/LeagueAdapter/Provider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Pugs\LeagueAdapter;
+namespace Pugs\Adapter\LeagueAdapter;
 
 class Provider
 {
