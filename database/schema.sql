d94d85683aef4e0c5e2433000b036c139884ee14
rmrhz <rmrhz@tfwno.gf>
2016-01-24 04:56:40 +0900

    Added groups table

--- database/schema.sql
+++ database/schema.sql
@@ -20,6 +20,15 @@ CREATE TABLE `user_groups` (
 	`deleted_at` datetime DEFAULT null
 ) Engine=InnoDB Charset=utf8;
 
+CREATE TABLE `groups` (
+	`id` int(11) UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+	`name` varchar(255) NOT NULL,
+	`description` varchar(255) NOT NULL,
+	`created_at` timestamp DEFAULT CURRENT_TIMESTAMP,
+	`updated_at` datetime DEFAULT null,
+	`deleted_at` datetime DEFAULT null
+) Engine=InnoDB Charset=utf8;
+
 CREATE TABLE `products` (
 	`id` int(11) UNSIGNED PRIMARY KEY AUTO_INCREMENT,
 	`name` varchar(255) NOT NULL,
