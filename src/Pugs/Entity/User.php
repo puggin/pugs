b2e613abcbea3361c631da58a566c11e6d4407f3
rmrhz <rmrhz@tfwno.gf>
2016-01-24 07:05:36 +0900

    Added first & last name to be fillable in User entity

--- src/Pugs/Entity/User.php
+++ src/Pugs/Entity/User.php
@@ -5,7 +5,7 @@ namespace Pugs\Entity;
 final class User extends \Pugs\Application\Model
 {
 	protected $fillable = [
-		'username', 'nickname', 'email'
+		'username', 'nickname', 'first_name', 'last_name' 'email'
 	];
 
 	protected $guarded = [
