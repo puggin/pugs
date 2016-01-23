d36de8c4f1f13ab4549ae0872a4d9118c3cb3c99
rmrhz <rmrhz@tfwno.gf>
2016-01-24 04:55:13 +0900

    Added group relationship to User entity

--- src/Pugs/Entity/User.php
+++ src/Pugs/Entity/User.php
@@ -11,4 +11,14 @@ final class User extends \Pugs\Application\Model
 	protected $guarded = [
 		'password'
 	];
+
+	/**
+	 * Retrieve all user groups 
+	 *
+	 * @return \Pugs\Entity\UserGroup
+	 */
+	public function groups()
+	{
+		return $this->hasMany('Pugs\Entity\UserGroup');
+	}
 }
