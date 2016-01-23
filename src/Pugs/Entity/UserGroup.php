83a50cc848eabe8f5b297797c79823dac0e378ba
rmrhz <rmrhz@tfwno.gf>
2016-01-24 05:00:47 +0900

    Added relationships to UserGroup entity

--- src/Pugs/Entity/UserGroup.php
+++ src/Pugs/Entity/UserGroup.php
@@ -4,5 +4,23 @@ namespace Pugs\Entity;
 
 final class UserGroup extends \Pugs\Application\Model
 {
-	
+	/**
+	 * Retrieve User information
+	 *
+	 * @return \Pugs\Entity\User
+	 */
+	public function user()
+	{
+		return $this->belongsTo('Pugs\Entity\User');
+	}
+
+	/**
+	 * Retrieve Group information
+	 *
+	 * @return \Pugs\Entity\Group
+	 */
+	public function group()
+	{
+		return $this->belongsTo('Pugs\Entity\Group');
+	}
 }
